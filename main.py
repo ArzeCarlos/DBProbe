75564ddbcbb575067ec3a914146703d850a41e74
ArzeCarlos <131378981+ArzeCarlos@users.noreply.github.com>
2023-05-29 11:43:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,21 @@
-from flask import Flask, render_template, request,redirect,url_for,session
-from flask_socketio import SocketIO
-from threading import Lock
-from datetime import datetime
-from paho.mqtt import client as mqtt_client
-from email.message import EmailMessage
-import random
-import json
 import pymysql
 import pymysql.cursors
-import re
-import ssl
-import smtplib
-user=''
-'''
-    EMAIL PARAMETERS
-'''
-email_sender='carlosarzez25@gmail.com'
-email_password='ohkkhnkblfckdmhn'
-email_receiver=''
-subject="Envio contraseña y password"
-body=""
-"""
-    MQTT BROKER PARAMETERS
-"""
-broker = 'ec2-44-204-178-128.compute-1.amazonaws.com'
-port = 1883
-topic = "WSNPIIData"
-client_id = f'python-mqtt-{random.randint(0, 100)}'
-username = 'carlos'
-password = 'carlos123'
-usernameDisplay=''
 def obtener_conexion():
     connection = pymysql.connect(host='localhost',
-                             user='root',
+                             user='carlos',
                              password='Carlos123#',
-                             database='WSNProjectII')
+                             database='1')
+    with connection.cursor() as cursor:
+    # Create a new record
+       sql = "INSERT INTO `DevicesData` (`value`) VALUES ('3')"
+       cursor.execute(sql)
+
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+    connection.commit()
     with connection:
         with connection.cursor() as cursor:
-            sql = "SELECT * FROM `user`"
+            sql = "SELECT * FROM `DevicesData`"
             cursor.execute(sql)
             result = cursor.fetchone()
             print(result)
