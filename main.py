b2bbe53a2c68c530f16fd8c1e89e671ff3c00b7b
ArzeCarlos <131378981+ArzeCarlos@users.noreply.github.com>
2023-05-29 12:20:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
+
+import pymysql
 import pymysql.cursors
-# Connect to the database
-connection = pymysql.connect(host='localhost',
+import re
+import ssl
+import smtplib
+user=''
+'''
+    EMAIL PARAMETERS
+'''
+email_sender='carlosarzez25@gmail.com'
+email_password='ohkkhnkblfckdmhn'
+email_receiver=''
+subject="Envio contraseña y password"
+body=""
+"""
+    MQTT BROKER PARAMETERS
+"""
+broker = 'ec2-44-204-178-128.compute-1.amazonaws.com'
+port = 1883
+topic = "WSNPIIData"
+client_id = f'python-mqtt-{random.randint(0, 100)}'
+username = 'carlos'
+password = 'carlos123'
+usernameDisplay=''
+def obtener_conexion():
+    connection = pymysql.connect(host='localhost',
                              user='root',
                              password='Carlos123#',
                              database='1')
-
-with connection:  
-    with connection.cursor() as cursor:
-        # Read a single record
-        sql = "SELECT * FROM devices"
-        cursor.execute(sql)
-        result = cursor.fetchone()
-        print(result)
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM `devices`"
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            print(result)
+if __name__ == '__main__':
+    obtener_conexion()
