7553a6c7b9cebe90590b5abc9f76d26d8f942582
ArzeCarlos <131378981+ArzeCarlos@users.noreply.github.com>
2023-05-29 12:16:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-import pymysql
 import pymysql.cursors
-def obtener_conexion():
-    connection = pymysql.connect(host='localhost',
-                             user='carlos',
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user='root',
                              password='Carlos123#',
                              database='1')
-    with connection.cursor() as cursor:
-    # Create a new record
-       sql = "INSERT INTO `DevicesData` (`value`) VALUES ('3')"
-       cursor.execute(sql)
 
-    # connection is not autocommit by default. So you must commit to save
-    # your changes.
-    connection.commit()
-    with connection:
-        with connection.cursor() as cursor:
-            sql = "SELECT * FROM `DevicesData`"
-            cursor.execute(sql)
-            result = cursor.fetchone()
-            print(result)
-if __name__ == '__main__':
-    obtener_conexion()
+with connection:  
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT * FROM devices"
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        print(result)
